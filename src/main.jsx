727a92c5a5c222f7d79cc38b52d0e4515df1884d
James Fraschilla <james.f.fraschilla@gmail.com>
2026-01-09 13:55:57 -0500

    Remove basename from HashRouter for root load

--- src/main.jsx
+++ src/main.jsx
@@ -18,7 +18,7 @@ const queryClient = new QueryClient({
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <HashRouter basename={import.meta.env.BASE_URL.replace(/\/$/, "")}>
+      <HashRouter>
         <App />
       </HashRouter>
     </QueryClientProvider>
